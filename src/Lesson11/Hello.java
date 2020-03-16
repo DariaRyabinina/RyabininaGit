219b75c325d9229b59f1c1ed914c186e81297c42
Daria <ryabinina@i-teco.ru>
2020-03-16 09:58:21 +0300

    changes2

--- src/Lesson11/Hello.java
+++ src/Lesson11/Hello.java
@@ -2,7 +2,6 @@ package Lesson11;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.print("Здравствуйте");
-        System.out.print("!");
+        System.out.print("Здравствуйте!");
     }
 }
