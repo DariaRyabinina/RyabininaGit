40d052c618476704bcfe5780fd1f05d19a8983d3
Daria <ryabinina@i-teco.ru>
2020-03-21 12:38:32 +0300

    changes3

--- src/Lesson11/Hi.java
+++ src/Lesson11/Hi.java
@@ -2,7 +2,7 @@ package Lesson11;
 
 public class Hi {
     public static void main(String[] args) {
-        System.out.print("Привет");
+        System.out.print("Привет, ");
         System.out.print("!!!");
     }
 }
